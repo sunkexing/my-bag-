74074823c99af2f8bbe5d0f1ba06717f3e6e73d6
sunkexing <1633896174@qq.com>
2019-12-16 18:05:29 +0800

    small to big

---
+++ 取较大值/取较大值/取较大值_输出一个多位数各位的数字.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int get_max(int x,int y) 
+//{
+//	
+//	return ( x > y ? x : y);
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 9;
+//	int max = get_max(a, b);
+//	printf("max=%d\n",max);
+//	return 0;
+//
+//}
+#include<stdio.h>
+#include<math.h>
+
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n/10);
+//	}
+//	printf("%d\n",n%10);
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d",&num);
+//	print(num);
+//	return 0;
+//}
+
+
