06dda537d8f6b614ddd8a2557275af441844d676
sunkexing <1633896174@qq.com>
2019-12-23 16:43:47 +0800

    12-23

--- 判断一个数是否为素数/判断一个数是否为素数/判断一个数为素数.c
+++ 判断一个数是否为素数/判断一个数是否为素数/判断一个数为素数.c
@@ -1,29 +1,68 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+//100-200之间的素数
+//int main()
+//{
+//  int a = 0;
+//  int b = 0;
+//  for (a = 100; a <= 200; a++)
+//  {
+//	  for (b = 2; b < a; b++)
+//	  {
+//		  if (a%b == 0)
+//			  break;
+//	  }
+//	  if (b == a)
+//      printf("%d\n", a);
+//   }
+//  return 0;
+//}
+//输入一个数，判断是否为素数
 
-
-int is_prime(int i)
-{
-	int j = 0;
-	for (j = 2; j <=sqrt(i) ; j++)
-	{
-		if (i%j == 0)
-			return 0;
-	}
-	return 1;
-
-}
-
-int main()
-{
-	int i = 0;
-	for (i = 1; i <= 100; i+=2)
-	{
-		if (is_prime == 1)
-		{
-
-			printf("%d", i);
-		}
-	
-	}
-	return 0;
-}
+//int main()
+//{
+//	int i,j,k;
+//	printf("请输入一个正整数\n");
+//	scanf("%d", &i);
+	//j = (int)sqrt(i);
+	//for (k = 2; k <= j; k++)
+	//{
+	//	if (i%k == 0)
+	//		break;
+	//}
+	//if (k > j)
+	//{
+	//	printf("是素数\n");
+	//}
+	//else
+	//	printf("不是素数\n");
+//	return 0;
+//}
+//int Is(int i)
+//{
+//	int j,k;
+//	j = (int)sqrt(i);
+//	for (k = 2; k <= j; k++)
+//	{
+//		if (i%k == 0)
+//			break;
+//	}
+//	if (k > j)
+//	{
+//		return 1;
+//	}
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	int i;
+//	scanf("%d", &i);
+//	if(Is (i)==1)
+//		printf("是素数\n");
+//	else
+//		printf("不是素数\n");
+//	return 0;
+//}
