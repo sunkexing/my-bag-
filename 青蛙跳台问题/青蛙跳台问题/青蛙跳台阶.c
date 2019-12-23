e1a286c4c4de00396ac8743db7d56f4592e2f582
sunkexing <1633896174@qq.com>
2019-12-23 17:12:09 +0800

    12-23

--- 青蛙跳台问题/青蛙跳台问题/青蛙跳台阶.c
+++ 青蛙跳台问题/青蛙跳台问题/青蛙跳台阶.c
@@ -1,47 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 //一次1阶，2阶，跳到n阶的种数
-//fib(int i)
-//{
-//	if (i == 1)
-//	{
-//		return 1;
-//	}
-//	else if (i == 2)
-//	{
-//		return 2;
-//	}
-//	else if (i > 2)
-//	{
-//		return fib(i - 1) + fib(i - 2);
-//	}
-//}
-//int main()
-//{
-//	int n;
-//	scanf("%d", &n);
-//	printf("%d\n",fib(n));
-//	return 0;
-//}
-//一次1阶，2阶，3阶...n阶，跳到n阶的种数
-
- fib(int i)
+int fib(int i)
 {
-	int ret;
 	if (i == 1)
 	{
-		printf("%d\n", i);
+		return 1;
 	}
-	if(i>=2)
+	else if (i == 2)
 	{
-		ret = 2 * fib(i - 1);
-		printf( "%d\n",ret);
+		return 2;
+	}
+	else if (i > 2)
+	{
+		return fib(i - 1) + fib(i - 2);
 	}
 }
 int main()
 {
 	int n;
 	scanf("%d", &n);
-	fib(n);
+	printf("%d\n",fib(n));
 	return 0;
 }
+//一次1阶，2阶，3阶...n阶，跳到n阶的种数
+
+
